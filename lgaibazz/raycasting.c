f8f04221d3af2d4ed9f831e14447fac92b35b75d
Luca Gaibazzi <lgaibazz@c1r2p5.42firenze.it>
2023-11-07 18:47:30 +0100

    raycaster sistemato (prototipo)

--- lgaibazz/raycasting.c
+++ lgaibazz/raycasting.c
@@ -11,147 +11,156 @@
 /* ************************************************************************** */
 #include "Cub3d.h"
 
-#define S_WIDTH 800
-#define S_HEIGHT 400
-#define M_WIDTH 800
-#define M_HEIGHT 600
-#define PLAYER_SIZE 1
+typedef struct s_data
+{
+	int		width;
+	int		height;
+	float	p_x;
+	float	p_y;
+	float	fov;
+	float	angle;
+	float	p_angle;
+	void	*mlx;
+	void	*win;
+	
+}	t_data;
 
-int		mapHeight = 8;
-int		mapWidth = 8;
-int		playerX;
-int		playerY;
-float	playerAngle;
-float	tileSize = 3;
-
-char	map[8][8] = {
-    {'1','1','1','1','1','1','1','1'},
-    {'1','0','0','0','0','0','0','1'},
-    {'1','0','S','0','1','1','0','1'},
-    {'1','0','0','0','0','0','0','1'},
-    {'1','0','1','0','1','1','0','1'},
-    {'1','0','0','0','0','1','0','1'},
-    {'1','0','1','1','0','0','0','1'},
-    {'1','1','1','1','1','1','1','1'}
+char	map[10][10] = {
+    "1111111111",
+    "1000000001",
+    "1000000001",
+    "1000000001",
+    "10000P0001",
+    "1000000001",
+    "1001001001",
+    "1000000001",
+    "1000000001",
+    "1111111111"
 };
 
-void render_scene(void *mlx_ptr, void *win_ptr)
+void	quitter(t_data *data)
 {
-    int x;
-    int y;
-
-    y = 0;
-    while (y < S_HEIGHT)
-    {
-        x = 0;
-        while (x < S_WIDTH)
-        {
-            int hitWall = 0;
-            float distanceToWall = 0;
-            float rayAngle = (playerAngle - 0.5) + ((float)x / (float)S_WIDTH);
-            float stepSize = 0.1;
-            float eyeX = sin(rayAngle);
-            float eyeY = cos(rayAngle);
-
-            while (!hitWall && distanceToWall < 16)
-            {
-                distanceToWall += stepSize;
-                int testX = (int)(playerX + eyeX * distanceToWall);
-                int testY = (int)(playerY + eyeY * distanceToWall);
-                if (testX < 0 || testX >= mapWidth || testY < 0 || testY >= mapHeight)
-                {
-                    hitWall = 1;
-                    distanceToWall = 16;
-                }
-                else if (map[testY][testX] == '1')
-                    hitWall = 1;
-            }
-            int ceiling = (S_HEIGHT / 2) - (S_HEIGHT / distanceToWall);
-            int floor = S_HEIGHT - ceiling;
-            if (y <= ceiling)
-                mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x000000); // Black color for ceiling
-            else if (y > ceiling && y <= floor)
-            {
-                float wallX = playerX + eyeX * distanceToWall;
-                float wallY = playerY + eyeY * distanceToWall;
-                float wallAngle = atan2(wallY - playerY, wallX - playerX);
-                if (wallAngle < 0)
-                    wallAngle += 2 * M_PI;
-                int wallColor;
-                if (wallAngle >= 0 && wallAngle < M_PI / 2)
-                    wallColor = 0xFF0000; // Red color for walls facing east
-                else if (wallAngle >= M_PI / 2 && wallAngle < M_PI)
-                    wallColor = 0x00FF00; // Green color for walls facing south
-                else if (wallAngle >= M_PI && wallAngle < 3 * M_PI / 2)
-                    wallColor = 0x0000FF; // Blue color for walls facing west
-                else
-                    wallColor = 0xFFFF00; // Yellow color for walls facing north
-                mlx_pixel_put(mlx_ptr, win_ptr, x, y, wallColor);
-            }
-            else
-                mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0x808080); // Gray color for floor
-            x++;
-        }
-        y++;
-    }
+	free(data);
+	exit(0);
 }
 
-int	move_player(int keycode)
+void	initializer(t_data *data)
 {
-	if (keycode == 123)
-		playerAngle -= 0.1;
-	if (keycode == 124)
-		playerAngle += 0.1;
-	return (1);
+	data->width = 1200;
+	data->height = 800;
+	data->p_x = 4.5;
+	data->p_y = 4.5;
+	data->fov = 90 * (M_PI / 180);
+	data->angle = 0;
+	data->p_angle = 0;
+	data->mlx = mlx_init();
+	data->win = mlx_new_window(data->mlx, data->width, data->height, "Cub3D");
 }
 
-int main(void)
+void	render(t_data *data)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	int		x;
+	int		y;
 
-    // Find player position and facing direction in the map
-    for (int i = 0; i < mapHeight; i++)
-    {
-        for (int j = 0; j < mapWidth; j++)
-        {
-            if (map[i][j] == 'N' || map[i][j] == 'S' || map[i][j] == 'E' || map[i][j] == 'W')
-            {
-                playerX = j * tileSize + tileSize / 2; // Place the player at the center of the tile horizontally
-                playerY = i * tileSize + tileSize / 2; // Place the player at the center of the tile vertically
-                if (map[i][j] == 'N')
-					playerAngle = 0;
-				else if (map[i][j] == 'S')
-					playerAngle = 3.14159;
-				else if (map[i][j] == 'E')
-					playerAngle = 1.5708;
-				else if (map[i][j] == 'W')
-					playerAngle = 4.71239;
-				break;
-            }
-        }
-    }
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, S_WIDTH, S_HEIGHT, "Cub3d");
+	x = -1;
+	while (++x < data->width)
+	{
+		double	angle = data->p_angle - (data->fov / 2) + (x / (double)data->width) * data->fov;
+		double	r_x = data->p_x;
+	 	double	r_y = data->p_y;
+		double	r_dir_x = cos(angle);
+		double	r_dir_y = sin(angle);
 
-    // Render della scena iniziale
-    render_scene(mlx_ptr, win_ptr);
+		// Perform raycasting and collision detection with the map.
+		int	hit_wall = 0;
+		while (!hit_wall)
+		{
+			int	m_x = (int)r_x;
+			int	m_y = (int)r_y;
+	
+			// Check if the ray hits a wall.
+			if (map[m_x][m_y] == '1')
+			{
+				hit_wall = 1;
+				// Calculate the distance from the player to the wall.
+				double	wall_dist = sqrt((r_x - data->p_x) * (r_x - data->p_x) + (r_y - data->p_y) * (r_y - data->p_y));
+				// Calculate the height of the wall segment based on the distance.
+				int	wall_height = (int)(data->height / wall_dist);
+	
+				// Calculate the position of the wall segment on the screen.
+				int	wall_screen_x = x;
+				int	wall_screen_height = wall_height;
+				// Calculate the top and bottom of the wall segment.
+				int	wall_screen_top = (data->height - wall_height) / 2;
+				int	wall_screen_bottom = (data->height + wall_height) / 2;
+				// Render the wall segment on the screen.
+				y = -1;
+				while (++y < data->height)
+				{
+					if (y < wall_screen_top)
+						mlx_pixel_put(data->mlx, data->win, x, y, 0x66FFFF); // Above the wall
+					else if (y >= wall_screen_top && y < wall_screen_bottom)
+						mlx_pixel_put(data->mlx, data->win, x, y, 0xFF0000); // Wall
+					else
+						mlx_pixel_put(data->mlx, data->win, x, y, 0x006600); // Below the wall
+				}
+			}
+			// Move the ray to the next grid square.
+			r_x += r_dir_x;
+			r_y += r_dir_y;
+		}
+	}
+}
 
-    // Event loop
-    while (true)
-    {
-        // Gestione rotazione e movimento player (non sono riuscito a finirlo!)
-		mlx_key_hook(win_ptr, move_player, NULL);
+int	idle_handler(t_data *data)
+{
+	render(data);
+}
+void	move_player(t_data *data, float dx, float dy)
+{
+    data->p_x += dx;
+    data->p_y += dy;
+    render(data);
+}
 
-        // Re-render the scene with updated player position and rotation
-        render_scene(mlx_ptr, win_ptr);
+void	rotate_player(t_data *data, float angle)
+{
+    data->p_angle += angle;
+    render(data);
+}
 
-        // Wait for the next frame
-        usleep(16667); // 60 FPS
-    }
-    // Close the window and cleanup
-    mlx_destroy_window(mlx_ptr, win_ptr);
-    mlx_destroy_display(mlx_ptr);
+int	handle_keypress(int keycode, t_data *data)
+{
+	float	move_speed = 0.1;
+	float	rotation_speed = 0.05;
 
-    return 0;
+	if (keycode == 65307)
+		quitter(data);
+	else if (keycode == 119)
+		move_player(data, move_speed * cos(data->p_angle), move_speed * sin(data->p_angle));
+	else if (keycode == 100)
+		move_player(data, -move_speed * sin(data->p_angle), move_speed * cos(data->p_angle));
+	else if (keycode == 115)
+		move_player(data, -move_speed * cos(data->p_angle), -move_speed * sin(data->p_angle));
+	else if (keycode == 97)
+		move_player(data, move_speed * sin(data->p_angle), -move_speed * cos(data->p_angle));
+	else if (keycode == 65361)
+		rotate_player(data, -rotation_speed);
+	else if (keycode == 65363)
+		rotate_player(data, rotation_speed);
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	*data;
+
+	data = malloc(sizeof(t_data));
+	initializer(data);
+	render(data);
+	mlx_key_hook(data->win, handle_keypress, data);
+	mlx_loop_hook(data->mlx, (int (*)(void *))idle_handler, data);
+	mlx_loop(data->mlx);
+	quitter(data);
+	return (0);
 }
